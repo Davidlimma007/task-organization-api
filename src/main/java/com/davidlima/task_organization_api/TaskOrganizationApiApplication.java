14448cfe79d23cd85e0627e60ebadeb7ec9de38d
David Lima <davidlimma05@gmail.com>
2026-05-07 20:50:25 -0300

    Feat: setup inicial com primeiras configurações do application.yaml

---
+++ src/main/java/com/davidlima/task_organization_api/TaskOrganizationApiApplication.java
@@ -0,0 +1,13 @@
+package com.davidlima.task_organization_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOrganizationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidlima/task_organization_api/TaskOrganizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidlima.task_organization_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOrganizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
